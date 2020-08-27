78dc680d7b5857d7a345377f20fb6d6ea558ddd5
Michael <michael.grigoryan25@gmail.com>
2020-08-27 10:33:43 +0400

    Added Google Maps API

--- public/js/issAPIWeb/script.js
+++ public/js/issAPIWeb/script.js
@@ -3,6 +3,8 @@ const longitude = document.getElementById("longitude");
 const latitude = document.getElementById("latitude");
 const timestamp = document.getElementById("timestamp");
 const apiStatusCont = document.getElementById("status");
+
+// URL For the API
 const url = "/iss-api";
 
 // Limit the API request speed
@@ -23,12 +25,14 @@ function liveUpdate() {
   data.send();
 
   function reqListener() {
-    longitude.innerHTML = this.response.details.iss_position.longitude
-    latitude.innerHTML = this.response.details.iss_position.latitude
-    timestamp.innerHTML = this.response.details.timestamp
-    apiStatusCont.innerHTML = this.response.apiStatus
+    longitude.innerHTML = this.response.details.iss_position.longitude;
+    latitude.innerHTML = this.response.details.iss_position.latitude;
+    timestamp.innerHTML = this.response.details.timestamp;
+    apiStatusCont.innerHTML = this.response.apiStatus;
   }
 }
 
 setInterval(liveUpdate, reqSpeed);
 console.log("%cConnected to the Server", "color: lightgreen");
+
+// Google Maps JS API
