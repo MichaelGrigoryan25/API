30b545bc4bbd31ba25fbabbc1536a751057e1507
Michael Grigoryan <michael.grigoryan25@gmail.com>
2020-08-28 15:06:04 +0000

    Updated the documentation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-var port = process.env.PORT || 80;
+const port = process.env.PORT || 8000;
 
 server.listen(port);
 console.log(`Listening at port: ${port}`)
